f8b045629659309fafb3b78bc0faf7584a665bc9
qkreltms <junghooncentralpark@gmail.com>
2023-10-02 00:06:17 +0900

    update: safe area 추가

--- lib/pages/Guide.dart
+++ lib/pages/Guide.dart
@@ -6,30 +6,31 @@ class PageGuide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Image(image: AssetImage('assets/images/google.png')),
-                const Text(
-                  'TITLE',
-                ),
-                const Text(
-                  'SUB_TITLe',
-                ),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.yellow,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                    onPressed: () {
-                      Get.toNamed('/select-my-info');
-                    },
-                    child: const Text('시작하기'))
-              ],
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Image(image: AssetImage('assets/images/google.png')),
+            const Text(
+              'TITLE',
             ),
-          ),
-        );
+            const Text(
+              'SUB_TITLe',
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                onPressed: () {
+                  Get.toNamed('/select-my-info');
+                },
+                child: const Text('시작하기'))
+          ],
+        ),
+      ),
+    ));
   }
 }
