e12a80821365ad236117ac8d73d589f04a7450d2
qkreltms <junghooncentralpark@gmail.com>
2023-10-09 18:53:30 +0900

    update: guide 페이지 슬라이드 가능하도록 변경

--- lib/pages/Guide.dart
+++ lib/pages/Guide.dart
@@ -1,35 +1,103 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:web_rtc_app/pages/SelectMyInfo.dart';
+import 'package:web_rtc_app/utils/LocalStorage.dart';
+
+class Guide1 extends StatelessWidget {
+  const Guide1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(children: [
+          const Text('Guide1.'),
+          FilledButton(
+              onPressed: () => scrollController.animateTo(
+                  MediaQuery.of(context).size.width - 100,
+                  curve: Curves.linear,
+                  duration: const Duration(microseconds: 1000)),
+              child: const Text('다음'))
+        ]));
+  }
+}
+
+class Guide2 extends StatelessWidget {
+  const Guide2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(children: [
+          const Text('Guide2.'),
+          FilledButton(
+              onPressed: () => scrollController.animateTo(
+                  MediaQuery.of(context).size.width * 2 - 100,
+                  curve: Curves.linear,
+                  duration: const Duration(microseconds: 1000)),
+              child: const Text('다음'))
+        ]));
+  }
+}
+
+class Guide3 extends StatelessWidget {
+  const Guide3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(children: [
+          const Text('Guide3.'),
+          FilledButton(
+              onPressed: () => scrollController.animateTo(0 - 100,
+                  curve: Curves.linear,
+                  duration: const Duration(microseconds: 1000)),
+              child: const Text('다음'))
+        ]));
+  }
+}
+
+ScrollController scrollController = ScrollController();
 
 class PageGuide extends StatelessWidget {
-  PageGuide({super.key});
+  const PageGuide({super.key});
 
+  final List<Widget> guides = const [
+    Guide1(),
+    Guide2(),
+    Guide3(),
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Image(image: AssetImage('assets/images/google.png')),
-            const Text(
-              'TITLE',
-            ),
-            const Text(
-              'SUB_TITLe',
-            ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.yellow,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                onPressed: () {
-                  Get.toNamed('/select-my-info');
-                },
-                child: const Text('시작하기'))
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('HAZE'),
+        actions: [
+          TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              ),
+              onPressed: () {
+                localStorage.setBool('enableGuide', false);
+                Get.to(const PageSelectMyInfo());
+              },
+              child: const Text('건너뛰기'))
+        ],
+      ),
+      body: ListView.builder(
+        controller: scrollController,
+        itemBuilder: (_, index) {
+          return guides[index];
+        },
+        scrollDirection: Axis.horizontal,
+        physics: const PageScrollPhysics(),
+        itemCount: guides.length,
       ),
     ));
   }
