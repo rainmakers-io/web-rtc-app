d8d9006d5861288fd466d557c63d51f5f33b93cb
qkreltms <junghooncentralpark@gmail.com>
2023-12-17 18:22:57 +0900

    refactor: 파일구조 규칙 추가

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 1. 클래스 명 
 [Folder name][Name]
 예: PageHome
+
+2. 파일 규칙
+```
+atoms - 가장 기본 단위가 되는 공통 컴포넌트
+molecules - atoms 컴포넌트 혹은 molecules 컴포넌트가 포함된 공통 컴포넌트, 하위에 그룹 폴더를 둘 수 있다(특정 비즈니스 종속 컴포넌트)
+template - 틀을 만들어주는 컴포넌트
+dialog - modal, alert, bottom sheet 등등 dialog 형태의 컴포넌트
+```
--- lib/pages/MatchingRoom.dart
+++ lib/pages/MatchingRoom.dart
@@ -10,51 +10,8 @@ import 'package:web_rtc_app/constants/Fonts.dart';
 import 'package:web_rtc_app/widgets/dialog/BottomSheetChooseTarget.dart';
 import 'package:web_rtc_app/widgets/atoms/FillButton.dart';
 import 'package:web_rtc_app/widgets/dialog/BottomSheetMatchingFilter.dart';
+import 'package:web_rtc_app/widgets/molecules/matching-room/SlideAnimation.dart';
 
-class SlideAnimation extends StatefulWidget {
-  Widget child;
-  final void Function(AnimationController ctl) onInit;
-
-  SlideAnimation({
-    super.key,
-    required this.child,
-    required this.onInit,
-  });
-
-  @override
-  State<StatefulWidget> createState() {
-    return _SlideAnimation();
-  }
-}
-
-class _SlideAnimation extends State<SlideAnimation>
-    with SingleTickerProviderStateMixin {
-  late Animation<Offset> _animation;
-  late AnimationController ctl;
-
-  @override
-  void initState() {
-    super.initState();
-    ctl = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 3000));
-    widget.onInit(ctl);
-
-    // 왼쪽 => 오른쪽을 이동하는 슬라이드 애니메이션F
-    Tween<Offset> _tween =
-        Tween(begin: const Offset(0.0, 0.0), end: const Offset(1.2, 0.0));
-    _animation = _tween.animate(
-      CurvedAnimation(
-        parent: ctl,
-        curve: Curves.easeInOut,
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SlideTransition(position: _animation, child: widget.child);
-  }
-}
 
 class PageMatchingRoom extends GetView<CtlMatchingRoom> {
   late AnimationController _animationController;
